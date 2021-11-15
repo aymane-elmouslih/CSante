1288c10371d245dbd1fc328b8ed9bd670ce28375
Kettani Amine <amine.kettani@student-cs.fr>
2021-11-15 16:07:35 +0100

    ajout de datacollec

---
+++ data_collection.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import csv
+   
+dep_population=pd.read_excel("/Users/utilisateur/Documents/CW_WEEK2/csante/Data/estim-pop-dep-sexe-gca-1975-2021.xlsx", sheet_name="2021",usecols=[0,1,7],skiprows=[0,1,2,3,101, 102,103,104,105,106,107,108,109,110], names=["Numdép", "Nomdép", "Population"])
+print(dep_population)
+   
+    
+    
