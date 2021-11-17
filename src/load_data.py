b20569264a83ec1b8cfa2b9f4949eca96231c5f9
aymane el mouslih <aymane.el-mouslih@student-cs.fr>
2021-11-17 11:17:22 +0100

    lapp

--- src/load_data.py
+++ src/load_data.py
@@ -1,9 +1,15 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
 import pandas as pd
 import json
-import plotly.express as px
 import plotly.io as pio
-pio.renderers.default = "browser"
-# Grouper les données par département et supprimer
+import plotly.graph_objects as go
+from dash import Dash, dcc, html, Input, Output
+
+app = dash.Dash(__name__)
+
 df = pd.read_excel(".\csante\Data\equip-serv-sante-com-2020.xlsx", skiprows=[0, 1, 2, 3, 5])
 Df = df.groupby('Département').sum()
 Df = Df.drop(['Région'], axis=1)  # les columns non utiles
@@ -21,21 +27,104 @@ for feature in map['features']:
 
 
 
-#fig = px.choropleth(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'])
+#fig = px.choropleth_mapbox(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'],
+#                           mapbox_style="carto-positron",
+ #                          center={"lat": 47, "lon": 2},
+  #                         zoom=5,opacity=0.8
+     #                     ,color_continuous_scale="Viridis")
 #fig.update_geos(fitbounds="locations", visible=False)
-#fig.show()
-
-px.choropleth_mapbox
-
-
-fig = px.choropleth_mapbox(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'],
-                           mapbox_style="carto-positron",
-                           center={"lat": 47, "lon": 2},
-                           zoom=5,opacity=0.7
-                          ,color_continuous_scale="Viridis")
-fig.update_geos(fitbounds="locations", visible=False)
-fig.show()
 
 
 
+
+
+
+
+
+app.layout = html.Div(children=[
+    
+                html.H1(children="Group 'ConnectionS'"),
+
+                
+
+                
+                html.Div(id="style",children=[]
+                     ),
+                
+                
+                
+
+                
+                dcc.Dropdown(id="slct_style",
+                 options=[
+                     {"label": "white-bg", "value": "white-bg"},
+                     {"label": "open-street-map", "value": "open-street-map"},
+                     {"label": "carto-positron", "value": "carto-positron"},
+                     {"label": "carto-darkmatter", "value": "carto-darkmatter"},
+                     {"label": "stamen-terrain", "value": "stamen-terrain"}],
+                 multi=False,
+                 value='carto-positron',
+                 style={'width': "40%"}
+                 ),
+                
+                
+                dcc.Dropdown(id="slct_couleure",
+                 options=[
+                     {"label": "px.colors.diverging.BrBG", "value": 1},
+                     {"label": "px.colors.sequential.Cividis_r", "value":2},
+                     {"label": "Viridis", "value": "Viridis"}],
+                 multi=False,
+                 value='Viridis',
+                 style={'width': "40%"}
+                 ),
+                
+                
+                
+                
+                dcc.Graph(
+                id='map',
+                figure={}
+                ),
+                
+                
+                
+                
+                
+                
+                
+ ])
+                          
+@app.callback(
+    [Output(component_id='style', component_property='children'),
+     Output(component_id='map', component_property='figure')],
+    [Input(component_id='slct_style', component_property='value'),
+     Input(component_id="slct_couleure", component_property='value')]
+)
+
+def update_graph(option_slctd1,option_slctd2):
+    if option_slctd2 == 1:
+             fig = px.choropleth_mapbox(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'],
+                                   mapbox_style=option_slctd1,
+                                   center={"lat": 47, "lon": 2},
+                                   zoom=5,opacity=0.8
+                                  ,color_continuous_scale=px.colors.diverging.BrBG)
+    elif  option_slctd2 == 2:
+         fig = px.choropleth_mapbox(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'],
+                                   mapbox_style=option_slctd1,
+                                   center={"lat": 47, "lon": 2},
+                                   zoom=5,opacity=0.8
+                                  ,color_continuous_scale=px.colors.sequential.Cividis_r)
+    else:
+        fig = px.choropleth_mapbox(Df, locations='id', geojson=map, color='Pharmacie', hover_name='Département_name',hover_data=['Établissement santé court séjour','Établissement santé moyen séjour','Établissement santé long séjour','Établissement psychiatrique','Centre lutte cancer','Urgences','Maternité','Centre de santé','Structures psychiatriques en ambulatoire','Centre médecine préventive','Dialyse','Hospitalisation à domicile','Maison de santé pluridisciplinaire',"Laboratoire d'analyses et de biologie médicale",'Ambulance','Transfusion sanguine','Établissement thermal','Pharmacie'],
+                                   mapbox_style=option_slctd1,
+                                   center={"lat": 47, "lon": 2},
+                                   zoom=5,opacity=0.8
+                                  ,color_continuous_scale='Viridis')
+         
+    fig.update_geos(fitbounds="locations", visible=False)
+    return option_slctd1,fig
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
 
