fa2f081402a0dc797c730edbdfa43917e4de2444
Kettani Amine <amine.kettani@student-cs.fr>
2021-11-17 17:24:16 +0100

    Simplification 2A 2B7

--- data_graphs.py
+++ data_graphs.py
@@ -10,33 +10,36 @@ fig=px.bar(data_collection.dep_pop(),x="Nomdép",y="Population")
 # On la récupère dans data_collection
 #le code suivant donne pour chaque département un visuel sous forme de camembert de la part de la population agée de plus de 65 ans
 
-def pie_chart(n): # n est le numéro du département souhaité. mettre 201 pour 2A et 202 pour 2B
-    if n<=19:
-       labels=["plus_de_65_ans","moins_de_65_ans"]
-       values=[data_collection.list_pourc_agés()[n-1],100-(data_collection.list_pourc_agés()[n-1])]
-       res=go.Figure(data=[go.Pie(labels=labels, values=values)])
-       res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département "+str(n))
-       res.show()
-    elif n==201:
+def pie_chart(n):# n est le STRING constitué du numéro du département souhaité.
+    if n!="2A" and n!="2B":
+       if int(n)<=19:
+           n=int(n)
+           labels=["plus_de_65_ans","moins_de_65_ans"]
+           values=[data_collection.list_pourc_agés()[int(n-1)],100-(data_collection.list_pourc_agés()[int(n-1)])]
+           res=go.Figure(data=[go.Pie(labels=labels, values=values)])
+           res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département "+str(n))
+           res.show()
+    
+       elif int(n)>=21:
+            labels=["plus_de_65_ans","moins_de_65_ans"]
+            values=[data_collection.list_pourc_agés()[int(n)],100-(data_collection.list_pourc_agés()[int(n)])]
+            res=go.Figure(data=[go.Pie(labels=labels, values=values)])
+            res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département " + str(n))
+            res.show()
+    elif n=='2A':
          labels=["plus_de_65_ans","moins_de_65_ans"]
          values=[data_collection.list_pourc_agés()[19],100-(data_collection.list_pourc_agés()[19])]
          res=go.Figure(data=[go.Pie(labels=labels, values=values)])
          res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département 2A")
          res.show()
-    elif n==202:
+    elif n=='2B':
          labels=["plus_de_65_ans","moins_de_65_ans"]
          values=[data_collection.list_pourc_agés()[20],100-(data_collection.list_pourc_agés()[20])]
          res=go.Figure(data=[go.Pie(labels=labels, values=values)])
          res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département 2B")
          res.show()
-    elif n>=21:
-         labels=["plus_de_65_ans","moins_de_65_ans"]
-         values=[data_collection.list_pourc_agés()[n],100-(data_collection.list_pourc_agés()[n])]
-         res=go.Figure(data=[go.Pie(labels=labels, values=values)])
-         res.update_layout(title_text="Part de la population agée de plus/moins de 65 ans dans le département " + str(n))
-         res.show()
         
     
-print(pie_chart(94))
+print(pie_chart("2A"))
     
     
