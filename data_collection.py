caf6beeddcf226dc76a98d125344570e58e6ba46
Kettani Amine <amine.kettani@student-cs.fr>
2021-11-17 16:41:09 +0100

    corrections

--- data_collection.py
+++ data_collection.py
@@ -8,7 +8,7 @@ Path2="./Data/equip-serv-sante-com-2020.xlsx"
  #   for n in df[x]:
   #      L.append(n)
 
-#le code retourne une dataframe avec la population par département
+#le code retourne une dataframe avec la population par département.
 
 def dep_pop():  
    dep_population=pd.read_excel("/Users/utilisateur/Documents/CW_WEEK2/csante/Data/estim-pop-dep-sexe-gca-1975-2021.xlsx", sheet_name="2021",usecols=[0,1,7],skiprows=[0,1,2,3,101, 102,103,104,105,106,107,108,109,110], names=["Numdép", "Nomdép", "Population"])
